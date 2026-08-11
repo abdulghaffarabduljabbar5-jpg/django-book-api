944bee6e4975f795138ed8f52ba6b1765eb59d24
Abdullah-bro 43 <jabb4343@gmail.com>
2026-08-11 18:14:13 +0500

    add all project files

---
+++ core/API/views.py
@@ -0,0 +1,22 @@
+from django.shortcuts import render
+from API.models import Book
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from API.serializer import BookSerializer
+# Create your views here.
+
+@api_view(['GET' , 'POST'])
+def BookList(request):
+    if request.method == 'GET':
+        book = Book.objects.all()
+        serializer = BookSerializer(book , many=True)
+        return Response({'Books': serializer.data})
+
+    if request.method == 'POST':
+        serializer = BookSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response({"Book is created": serializer.data})
+        return Response(serializer.errors)
+
+
