c411d9ee724e14e71d253c863cd530be1952581d
Abdullah-bro 43 <jabb4343@gmail.com>
2026-08-11 22:15:08 +0500

    Update views and urls code implementation

--- core/API/urls.py
+++ core/API/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from API.views import BookList
 
 urlpatterns = [
-    path('book/' , BookList , name='Book-list')
+    path('book/' , BookList.as_view() , name='Book-list')
 ]
--- core/API/views.py
+++ core/API/views.py
@@ -3,20 +3,23 @@ from API.models import Book
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from API.serializer import BookSerializer
+from rest_framework import generics
 # Create your views here.
 
-@api_view(['GET' , 'POST'])
-def BookList(request):
-    if request.method == 'GET':
-        book = Book.objects.all()
-        serializer = BookSerializer(book , many=True)
-        return Response({'Books': serializer.data})
+# @api_view(['GET' , 'POST'])
+# def BookList(request):
+#     if request.method == 'GET':
+#         book = Book.objects.all()
+#         serializer = BookSerializer(book , many=True)
+#         return Response({'Books': serializer.data})
 
-    if request.method == 'POST':
-        serializer = BookSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response({"Book is created": serializer.data})
-        return Response(serializer.errors)
+#     if request.method == 'POST':
+#         serializer = BookSerializer(data=request.data)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response({"Book is created": serializer.data})
+#         return Response(serializer.errors)
 
-
+class BookList(generics.ListCreateAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
